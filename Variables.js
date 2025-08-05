b934a78da3c4abfd779e7b93eb30d19fd97982bf
Sriram Saiteja <70952994+SriramSaitejaOfficial@users.noreply.github.com>
2025-08-05 23:18:35 +0530

    modified file: Variables.js

--- Variables.js
+++ Variables.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+//console.log("Hello World");
+//The following code calculates the sum of two variables and logs the result to the console.
+
+var a = 10
+var b = 20;
+var c = a + b;
+console.log("The sum of a and b is: " + c); 
