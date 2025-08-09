7d0eb981b7e2d38ff7d4e3acf4530f51b8ec4257
Sriram Saiteja <70952994+SriramSaitejaOfficial@users.noreply.github.com>
2025-08-09 23:02:10 +0530

    Constants example in JavaScript // Constants are variables that cannot be reassigned after their initial assignment. // They are declared using the const keyword.

---
+++ Constant.js
@@ -0,0 +1,6 @@
+const christmas_2025='25 December 2025';
+
+console.log(christmas_2025);
+
+//christmas_2025 = '25 December 2026'; - This will throw an error because christmas_2025 is a constant and cannot be reassigned.
+
--- Variables.js
+++ Variables.js
@@ -1,7 +1,19 @@
-//console.log("Hello World");
-//The following code calculates the sum of two variables and logs the result to the console.
+//Variable is like a container. Every variable has a unique name assigned to it.
+//Variables can be used to store data, which can be accessed and modified later in the program
+//Variables can also be initialized with let keyword.
+//We usually follow camelCase naming convention for variables.
 
-var a = 10
-var b = 20;
-var c = a + b;
-console.log("The sum of a and b is: " + c); 
+var firstName = "John"; //String variable
+
+//Programmers refer to this kind of data as a string, which is a sequence of characters. These are enclosed by quotes.
+
+//Reserved keywords cannot be used as variable names.
+//Modern way of declaring variables is using let keyword.
+ 
+
+
+
+
+
+
+
