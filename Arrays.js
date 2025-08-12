8901c62d913b084d241b889be45b0ddf725e0f6a
Sriram Saiteja <70952994+SriramSaitejaOfficial@users.noreply.github.com>
2025-08-12 15:12:46 +0530

    Arrays example in JavaScript

---
+++ Arrays.js
@@ -0,0 +1,6 @@
+let studentDetails = ['name: Sriram Saiteja', 'age: 30', 'grade: A', 'subjects: Javascript, Python, C++'];
+
+console.log(studentDetails[0]);
+
+//To know the length of the array, you can use the length property
+console.log(studentDetails.length);
